bd45551568ef90a71e60dd4c7e8c2cc48c91f90c
Nguyễn Văn Liêm - 60135923 <nvliemc4ncthanh@gmail.com>
2022-04-10 08:46:19 +0700

    Initial commit

---
+++ src/Components/intro/Intro.jsx
@@ -0,0 +1,29 @@
+import './intro.scss'
+import { init } from 'ityped'
+import {useEffect, useRef} from "react"
+
+export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, { showCursor: false, strings: ['Backend Developer', '.NET Developer', "PHP Developer" ] })
+  },[])
+  return (
+    <div className='intro' id='intro'>
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/avtnew.png" alt="My avatar" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+              <h2>Hi There, I'm</h2>
+              <h1>Nguyen Van Liem</h1>
+              <h3>I work as a <span ref={textRef}></span></h3>
+          </div>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt="" />
+          </a>
+        </div>
+    </div>
+  )
+}
