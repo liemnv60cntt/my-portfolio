b7cdafe3a29fde54bdab0a9010b1a012af85b3fe
Nguyễn Văn Liêm - 60135923 <nvliemc4ncthanh@gmail.com>
2022-04-10 12:24:31 +0700

    Completed the portfolio

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,29 +1,33 @@
 import './intro.scss'
 import { init } from 'ityped'
-import {useEffect, useRef} from "react"
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
   const textRef = useRef();
   useEffect(() => {
-    init(textRef.current, { showCursor: false, strings: ['Backend Developer', '.NET Developer', "PHP Developer" ] })
-  },[])
+    init(textRef.current, { 
+      showCursor: true, 
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Backend Developer', '.NET Developer', "PHP Developer"] })
+  }, [])
   return (
     <div className='intro' id='intro'>
-        <div className="left">
-          <div className="imgContainer">
-            <img src="assets/avtnew.png" alt="My avatar" />
-          </div>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/avtnew.png" alt="My avatar" />
         </div>
-        <div className="right">
-          <div className="wrapper">
-              <h2>Hi There, I'm</h2>
-              <h1>Nguyen Van Liem</h1>
-              <h3>I work as a <span ref={textRef}></span></h3>
-          </div>
-          <a href="#portfolio">
-            <img src="assets/down.png" alt="" />
-          </a>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Nguyen Van Liem</h1>
+          <h3>I work as a <span ref={textRef}></span></h3>
         </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
